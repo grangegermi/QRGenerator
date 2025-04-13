71ea0a6136911a0efde06509220872000500bc15
grangegermi <dasha.voloshina.9883@gmail.com>
2025-04-13 16:55:46 +0300

    add file project

---
+++ QRGenerator/QRGenerator/QRGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRGeneratorApp.swift
+//  QRGenerator
+//
+//  Created by Даша Волошина on 13.04.25.
+//
+
+import SwiftUI
+
+@main
+struct QRGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
